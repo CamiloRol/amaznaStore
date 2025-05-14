0c8efcfc51632d137ed85f77bf006db53d8364c8
Manuela <manuelamunozlujan0@gmail.com>
2025-05-14 16:48:09 -0500

    Plantilla dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import Home from './pages/Home/Home'
-import { Route, Routes } from 'react-router-dom'
+import { DashboradPage } from "./admin/pages/DashboradPage";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <Routes>
-      <Route path='/' element={ <Home />}/>
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<DashboradPage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
