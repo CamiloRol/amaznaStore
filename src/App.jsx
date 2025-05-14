88d62b447f35f68dc87fbb43ceb62f692a3c6985
Israel <cuadrosisrael947@gmail.com>
2025-05-14 16:50:58 -0500

    se crea Register , rutas y de mas , se aplica tailwind para las propiedades , se ajusta login , inputs de grey a white font the white a black , se enruta en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import Home from './pages/Home/Home'
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+
 
 function App() {
 
   return (
     <Routes>
+      
       <Route path='/' element={ <Home />}/>
       <Route path="/Login" element={<LoginPage />} />
+      <Route path="/Register" element={<RegisterPage />} />
+
     </Routes>
   )
 }
