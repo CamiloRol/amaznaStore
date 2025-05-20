ef17cc7a200e9fa1a38df6127c418941bb161e13
Manuela <manuelamunozlujan0@gmail.com>
2025-05-20 09:35:31 -0500

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import { DashboradPage } from "./admin/pages/DashboradPage";
 import Home from "./pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
+import { ProtecterRoute } from "./admin/components/ProtecterRoute";
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    // Peticion al backend real
+    setUser({
+      id: 1,
+      name: "Manu"
+    })
+  };
+
+  const logout = () => setUser(null);
+
   return (
     <>
+
+    {
+      user? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <button onClick={login}>Login</button>
+      )
+    }
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<DashboradPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/dashboard" element={
+          <ProtecterRoute user={user}>
+            <DashboradPage/>
+          </ProtecterRoute>
+          } />
       </Routes>
     </>
   );
