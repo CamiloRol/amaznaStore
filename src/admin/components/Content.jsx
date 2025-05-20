d36a7e6e59e3aff841edaf4d85597516ac95d7d2
Camilo Rodriguez <arodriguez@cesde.net>
2025-05-20 11:05:14 -0500

    merging develiperMinu

--- src/admin/components/Content.jsx
+++ src/admin/components/Content.jsx
@@ -10,11 +10,11 @@ export const Content = () => {
         <div id="content"></div>
         <div className="container mx-auto">
           {/* Page Heading */}
-          <div className="flex sm:flex-row flex-col justify-between items-center mb-2 mt-2">
+          <div className="flex sm:flex-row flex-col justify-between items-center mb-2 mt-2 ml-3">
             <h1 className="text-xl font-semibold text-gray-100">Dashboard</h1>
             <a
               href="#"
-              className="hidden sm:inline-block btn btn-sm bg-blue-500 text-white shadow-sm px-3 py-2 rounded-md hover:bg-blue-600 transition"
+              className="hidden sm:inline-block btn btn-sm bg-blue-500 text-white shadow-sm px-3 py-2 rounded-md hover:bg-blue-600 transition mr-3"
             >
               <i className="fas fa-download fa-sm text-white opacity-50"></i>{" "}
               Generate Report
