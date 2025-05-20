b994f6c893fda31ba114dfc750d0784ee2ceaecb
Manuela <manuelamunozlujan0@gmail.com>
2025-05-20 10:51:14 -0500

    ProtectedRoute funcional

--- src/admin/components/ProtecterRoute.jsx
+++ src/admin/components/ProtecterRoute.jsx
@@ -1,9 +1,8 @@
 import { Navigate } from "react-router-dom";
-import Home from "../../pages/Home/Home";
 
 export const ProtecterRoute = ({user, children}) => {
     if(!user){
-        return <Navigate to={<Home/>} />
+        return <Navigate to="/" />
     }
 
   return children
