afe3006df69ada99e264fab918f54321965959ad
Israel <cuadrosisrael947@gmail.com>
2025-05-23 20:23:33 -0500

    ajustes para las cartas del banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,7 @@ import Products from "./products/products"
 export default function Banner() {
   return (
     <>
+    
     <div className="w-full h-full">
       <img
         src="/Banner.png"
@@ -12,6 +13,16 @@ export default function Banner() {
     </div>
     <div>
       <Products />
+      {/* <div className="absolute top-150 left-25 w-72 bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+    <div className="p-4">
+    <img src="/Banner.png" alt="Imagen del producto" />
+    <h2 className="text-xl font-semibold text-gray-800 mb-2">hola</h2>
+    <p className="text-gray-600 text-sm mb-4">hola</p>
+    <div className="text-right">
+      <span className="text-lg font-bold text-indigo-600">COP $5000</span>
+    </div>
+    </div>
+    </div> */}  
     </div>
     </>
   )
--- src/components/products/IndiProduct.jsx
+++ src/components/products/IndiProduct.jsx
@@ -1,9 +1,29 @@
 export default function IndiProduct({product}) {
-  return (
-    <div>
-      <h2>{product.produname}</h2>
-      <p>{product.description}</p>
-      <p>Precio: ${product.price}</p>
+ 
+ 
+return (
+
+    <div className="relative h-[900px]">
+      {product.slice(0, 6).map((product, index) => (
+        <div
+          key={index}
+          className="absolute w-72 bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+          style={{
+            top: 150 + Math.floor(index / 3) * 300, // 150px de margen superior maracaibo
+            left: 25 + (index % 3) * 280 // 25px de margen izquierdo
+          }}
+        >
+          <div className="p-4">
+            <h2 className="text-xl font-semibold text-gray-800 mb-2">{product.produname}</h2>
+            <p className="text-gray-600 text-sm mb-4">{product.description}</p>
+            <div className="text-right">
+              <span className="text-lg font-bold text-indigo-600">COP ${product.price}</span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
+
+
